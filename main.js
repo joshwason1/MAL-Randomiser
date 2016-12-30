4fda300e7c3f09fe23a842810404f5989a96e234
Josh Wason <joshwason1@hotmail.com>
2016-12-30 16:01:29 +1100

    Returns additional information
    Searches series information from MyAnimeList, including the synopsis,
    score, rank, popularity, type of series, episodes and PV.

--- main.js
+++ main.js
@@ -8,12 +8,29 @@ app.use(express.static('.'));
 
 app.get('/:username', function (req, res) {
     var username = req.params.username;
-    var user = "Username: " + username;
     var result;
     var $ = cheerio.load(fs.readFileSync("index.html"));
     $('#username').attr('value', username).html();
     mal.randomAnime(function(result) {
-        $('#result').html(result);
+        $('#title').attr('href', result.animeURL).html(result.title);
+        $('#img').attr('src', result.img).html();
+        $('#score').html("Score: " + result.score + " rated by " + result.ratingCount + " users.");
+        $('#description').html(result.description);
+        $('#ranked').html("Ranked: " + result.ranked);
+        $('#popularity').html("Popularity: " + result.popularity);
+        $('#members').html("Members: " + result.members);
+        $('#type').html("Type: " + result.type);
+        $('#eps').html("Episodes: " + result.eps);
+        if (result.aired !== "N/A") {
+            $('#aired').html("Premiered: " + result.aired);
+        } else {
+            $('#aired').remove();
+        }
+        if (result.pv !== "N/A") {
+            $('#pv').attr('src', result.pv).html();
+        } else {
+            $('#pv').remove();
+        }
         res.send($.html())
     }, username);
 });
--- mal.js
+++ mal.js
@@ -4,26 +4,66 @@ var request = require('request'),
 var username;
 var MALinfo;
 var status;
-var series_titles;
+var ids;
+var result;
+var resultID;
+var animeURL;
 
 exports.randomAnime = function(callback, username) {
     MALinfo = "https://myanimelist.net/malappinfo.php?u=" + username + "&status=all&type=anime";
     status = "2";
-    series_titles = [];
-    request(MALinfo, function(err, resp, body) {
+    ids = [];
+    request(MALinfo, function(error, response, body) {
         var $ = cheerio.load(body, {xmlMode: true});
         $('anime').each(function () {
             if ($(this).find('my_status').text() === status) {
-                series_titles.push($(this).find('series_title').text());
+                ids.push($(this).find('series_animedb_id').text());
             }
         });
-        if (series_titles.length > 0) {
-            var i = Math.floor((Math.random() * series_titles.length));
-            var result = series_titles[i];
-            console.log("Result: " + result);
-        } else {
-            console.log("No results found.")
+        if (ids.length > 0) {
+            var i = Math.floor((Math.random() * ids.length));
+            resultID = ids[i];
         }
-        callback(result);
-    })
+        animeURL = "https://myanimelist.net/anime/" + resultID;
+        request(animeURL, function (error, response, body) {
+            let $$ = cheerio.load(body);
+            let title = $$('.ac').first().attr('alt');
+            let img = $$('.ac').first().attr('src');
+            let score = $$('[itemprop="ratingValue"]').text();
+            let description = $$('[itemprop="description"]').text();
+            let ranked = $$('strong', '.numbers.ranked').text();
+            let popularity = $$('strong', '.numbers.popularity').text();
+            let members = $$('strong', '.numbers.members').text();
+            let ratingCount = $$('[itemprop="ratingCount"]').text();
+            let eps = $$('.spaceit:contains("Episodes")').text().trim().replace('Episodes:', '').trim();
+            let pv = 'N/A';
+            if ($$('.video-promotion').length) {
+                pv = `${$$('.video-promotion a').attr('href').substring(0, $$('.video-promotion a').attr('href').indexOf('?'))}`
+            }
+            let type = "N/A";
+            if ($$('a[href^="https://myanimelist.net/topanime.php?type="]').length) {
+                type = $$('a[href^="https://myanimelist.net/topanime.php?type="]').first().text();
+            }
+            let aired = "N/A";
+            if ($$('a[href^="https://myanimelist.net/anime/season/"]').length) {
+                aired = $$('a[href^="https://myanimelist.net/anime/season/"]').first().text();
+            }
+            result = {
+                animeURL: animeURL,
+                title: title,
+                img: img,
+                score: score,
+                description: description,
+                ranked: ranked,
+                popularity: popularity,
+                members: members,
+                ratingCount: ratingCount,
+                type: type,
+                eps: eps,
+                aired: aired,
+                pv: pv
+            };
+            callback(result);
+        });
+    });
 };
